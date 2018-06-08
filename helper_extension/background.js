ceeb38644cdb8faeb4cbf805f64433ab404fd145
Clark Xie <14343418+xiecy@users.noreply.github.com>
2018-06-08 09:43:00 -0400

    Update background.js

--- helper_extension/background.js
+++ helper_extension/background.js
@@ -1,8 +1,6 @@
-// this is the background code...
-
-// listen for our browerAction to be clicked
+// Listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
+	// For the current tab, inject the "inject.js" file and execute it
 	chrome.tabs.executeScript(tab.ib, {
 		file: 'inject.js'
 	});
